433793ae4ccfe9229d41667e752f76f3499b018b
JonasOliveira <133276203+JonasMaia08@users.noreply.github.com>
2026-01-04 14:05:49 +0000

    fix entities

--- api/src/entities/athlete.entity.ts
+++ api/src/entities/athlete.entity.ts
@@ -1,14 +1,8 @@
-import { Column, Entity, ManyToMany, OneToMany } from "typeorm";
+
+import { Column, Entity, OneToMany } from "typeorm";
 import { UserBase } from "./user-base.entity";
 import { Atendiment } from "./atendiment.entity";
-import { Modality } from "./modality.entity";
-<<<<<<< HEAD
-import { AthleteToModality } from "./athleteToModality.entity";
-=======
-import { ManyToOne } from "typeorm";
 import { Enrollment } from "./enrollment.entity";
-import { OneToMany } from "typeorm";
->>>>>>> af7abc0 (feat: envio e recebimento da lista de chamadas)
 
 @Entity("athlete")
 export class Athlete extends UserBase {
@@ -60,20 +54,9 @@ export class Athlete extends UserBase {
   @Column("text", { nullable: true, default: "Nenhuma alergia informada" })
   allergy: string;
 
-  @ManyToMany(() => Atendiment, (atendiment) => atendiment.athletes, {})
+  @OneToMany(() => Atendiment, (a) => a.athlete, { nullable: true })
   atendiments: Atendiment[];
 
-<<<<<<< HEAD
-    @ManyToMany(() => Modality, (modality) => modality.registred_athletes)
-    modalities: Modality[];
-
-    @OneToMany(() => AthleteToModality, (athleteToModality) => athleteToModality.athlete)
-    athleteToModality: AthleteToModality[];
-=======
-  @ManyToMany(() => Modality, (modality) => modality.registred_athletes)
-  modalities: Modality[];
-
-  @OneToMany(() => Enrollment)
+  @OneToMany(() => Enrollment, (e) => e.athlete, { nullable: true })
   enrollments: Enrollment[];
->>>>>>> af7abc0 (feat: envio e recebimento da lista de chamadas)
 }
--- api/src/entities/modality.entity.ts
+++ api/src/entities/modality.entity.ts
@@ -1,24 +1,21 @@
-import { DaysOfWeek } from "../enums/daysOfWeek.enum";
+
 import {
   Column,
   Entity,
-  ManyToMany,
   OneToMany,
   PrimaryGeneratedColumn,
 } from "typeorm";
 import { Teacher } from "./teacher.entity";
-import { Athlete } from "./athlete.entity";
-<<<<<<< HEAD
-import { AthleteToModality } from "./athleteToModality.entity";
-=======
-import { ManyToOne } from "typeorm";
 import { Enrollment } from "./enrollment.entity";
->>>>>>> af7abc0 (feat: envio e recebimento da lista de chamadas)
+import { Atendiment } from "./atendiment.entity";
 
 @Entity("modality")
 export class Modality {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({default:true})
+  active:boolean;
 
   @Column("text")
   name: string;
@@ -26,8 +23,12 @@ export class Modality {
   @Column("text")
   description: string;
 
-  @Column({ type: "text", array: true })
-  days_of_week: string[];
+  @Column({ type: "text"})
+  days_of_week: string;
+
+  get daysOfWeekArray() {
+    return this.days_of_week.split(',');
+  }
 
   @Column({ type: "time" })
   start_time: string;
@@ -35,25 +36,21 @@ export class Modality {
   @Column({ type: "time" })
   end_time: string;
 
-  @Column({ type: "text", array: true })
-  class_locations: string[];
+  @Column({ type: "text"})
+  class_locations: string;
+
+  get classLocationsArray() {
+    return this.class_locations.split(',');
+  }
 
   @OneToMany(() => Teacher, (teacher) => teacher.modality, {
     onDelete: "SET NULL",
   })
   teachers: Teacher[];
 
-<<<<<<< HEAD
-    @ManyToMany(() => Athlete, (athlete) => athlete.modalities)
-    registred_athletes: Athlete[];
+  @OneToMany(() => Atendiment, (a) => a.modality)
+  atendiments: Atendiment[];
 
-    @OneToMany(() => AthleteToModality, (athleteToModality) => athleteToModality.modality)
-    athleteToModality: AthleteToModality[];
-=======
-  @ManyToMany(() => Athlete, (athlete) => athlete.modalities)
-  registred_athletes: Athlete[];
-
-  @OneToMany(() => Enrollment)
+  @OneToMany(() => Enrollment, (e) => e.modality)
   enrollments: Enrollment[];
->>>>>>> af7abc0 (feat: envio e recebimento da lista de chamadas)
 }
